1e3535970398fae433357375bf2b1291997a2766
Mirko <mirkoj@gmail.com>
2017-01-08 18:07:31 -0500

    Added cocoa pods (Alamofire).

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'LucasBot' do
+    pod 'Alamofire', '~> 4.2'
+end
---
+++ Pods/Target Support Files/Pods-LucasBot/Pods-LucasBot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LucasBot : NSObject
+@end
+@implementation PodsDummy_Pods_LucasBot
+@end
