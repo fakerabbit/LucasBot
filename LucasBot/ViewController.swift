58e0a573f2386572c6f7be3665dae56789e50978
Mirko <mirkoj@gmail.com>
2017-01-08 17:32:58 -0500

    Initial Xcode project setup.

---
+++ LucasBot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LucasBot
+//
+//  Created by Mirko Justiniano on 1/8/17.
+//  Copyright © 2017 LB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
